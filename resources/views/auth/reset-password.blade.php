139a5c6ca5ba24b9203013b0e9d7f99fb40f751e
ValerikGl <valeri.gladunik@tptlive.ee>
2025-11-19 22:38:54 +0200

    Rewrite all Auth pages to DaisyUI

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,39 +1,54 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
+@extends('partials.layout')
+@section('title', 'Reset Password')
+@section('content')
+<div class="card bg-base-300 max-w-md mx-auto mt-10">
+    <div class="card-body">
+        <form method="POST" action="{{ route('password.store') }}">
+            @csrf
 
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+            <!-- Password Reset Token -->
+            <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+            <!-- Email Address -->
+            <fieldset class="fieldset">
+                <legend class="fieldset-legend">{{ __('Email') }}</legend>
+                <input id="email" name="email" type="email" required autofocus autocomplete="username"
+                       value="{{ old('email', $request->email) }}"
+                       class="input w-full @error('email') input-error @enderror"
+                       placeholder="Email" />
+                @error('email')
+                    <p class="label text-error mt-2">{{ $message }}</p>
+                @enderror
+            </fieldset>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+            <!-- Password -->
+            <fieldset class="fieldset mt-4">
+                <legend class="fieldset-legend">{{ __('Password') }}</legend>
+                <input id="password" name="password" type="password" required autocomplete="new-password"
+                       class="input w-full @error('password') input-error @enderror"
+                       placeholder="Password" />
+                @error('password')
+                    <p class="label text-error mt-2">{{ $message }}</p>
+                @enderror
+            </fieldset>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <!-- Confirm Password -->
+            <fieldset class="fieldset mt-4">
+                <legend class="fieldset-legend">{{ __('Confirm Password') }}</legend>
+                <input id="password_confirmation" name="password_confirmation" type="password" required autocomplete="new-password"
+                       class="input w-full @error('password_confirmation') input-error @enderror"
+                       placeholder="Confirm Password" />
+                @error('password_confirmation')
+                    <p class="label text-error mt-2">{{ $message }}</p>
+                @enderror
+            </fieldset>
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+            <div class="flex justify-end mt-4">
+                <button type="submit" class="btn btn-primary">
+                    {{ __('Reset Password') }}
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
